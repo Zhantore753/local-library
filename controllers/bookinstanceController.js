50539b0329124a0d248ddef91a485f4e816dd516
Zhantore <753gg@mail.ru>
2021-04-11 08:24:31 +0600

    controllers

---
+++ controllers/bookinstanceController.js
@@ -0,0 +1,42 @@
+// импорт модуля модели книги
+let BookInstance = require('../models/bookinstance');
+
+// Показать список всех книг.
+exports.bookinstance_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: BookInstance list');
+};
+
+// Показать детальную информацию по определенной книге.
+exports.bookinstance_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+};
+
+// Показать форму создания книги по запросу GET.
+exports.bookinstance_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: BookInstance create GET');
+};
+
+// Создать книгу по запросу POST.
+exports.bookinstance_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: BookInstance create POST');
+};
+
+// Показать форму удаления книги по запросу GET.
+exports.bookinstance_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+};
+
+// Удалить книгу по запросу POST.
+exports.bookinstance_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: BookInstance delete POST');
+};
+
+// Показать форму обновления книги по запросу GET.
+exports.bookinstance_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: BookInstance update GET');
+};
+
+// Обновить книгу по запросу POST.
+exports.bookinstance_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: BookInstance update POST');
+};
